731665833d81fd5121bb9873f852de0c4b8b3282
MarzukaaZaki <zakimarzuka@gmail.com>
2023-11-18 20:48:51 +0600

    Added a tab panel with mobile view

---
+++ src/components/TabPanel/TabPanel.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './TabPanel.css'
+const TabPanel = () => {
+    return (
+        <>
+         {/* Desktop and Laptop View Start */}
+            <div class="container posts-tab-container text-left mt-4 d-none d-md-block">
+                <div class="row d-flex">
+                    <div class="col-12 col-lg-8 mb-3 flex-start">
+                        <ul class="nav nav-underline">
+                            <li class="nav-item">
+                                <button class="nav-link active" aria-current="page" href="#">All Jobs(32)</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link" href="#">Article</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link" href="#">Event</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link" href="#">Education</button>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <div class="col-12 col-lg-4 text-primary my-3 my-md-0">
+                        <button class="btn bg-secondary-subtle me-2 text-black">Write Post <i
+                            class="fa-solid fa-caret-down ms-2"></i></button>
+                        <button class="btn btn-primary"><i class="fa-solid fa-user-group me-2"></i> Join Group</button>
+                    </div>
+                    <hr style={{width:"95%"}} class="ms-3" />
+                </div>
+
+
+            </div>
+            {/* Desktop and Laptop View End */}
+
+            {/* Tablet and Mobile View Start  */}
+            <div class="posts-tab-container mx-4 d-flex justify-content-between d-block mt-3 d-md-none">
+                <div>
+                    <p class="fw-bold"> Posts(368)</p>
+                </div>
+                <div>
+                    <button class="border border-0 rounded bg-secondary-subtle px-3 py-2"> Filter: All  <i
+                        class="fa-solid fa-caret-down ms-2"></i></button>
+                </div>
+
+            </div>
+            {/* Tablet and Mobile View End */}
+        </>
+    );
+};
+
+export default TabPanel;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
+import TabPanel from '../../components/TabPanel/TabPanel';
 
 const Home = () => {
     return (
         <div>
            <Header/>
            <Banner/>
+           <TabPanel/>
             This is tab panel and buttons.
             This is main content posts in the middle & groups on the side.
             
