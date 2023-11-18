4cd4c0ac9af96d5f7d1df9dce481cd5f28660b06
MarzukaaZaki <zakimarzuka@gmail.com>
2023-11-18 12:56:52 +0600

    Set up react project with bootstrap

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            This is home page.
+            This is navbar.
+            This is banner.
+            This is tab panel and buttons.
+            This is main content posts in the middle & groups on the side.
+            
+        </div>
+    );
+};
+
+export default Home;
